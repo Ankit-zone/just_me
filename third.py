9dba72996bce1740db02774c102ea9f4fdc59824
Ankit-zone <ankitankityadavgkp@gmail.com>
2025-01-10 08:09:22 +0530

    Add files via upload

---
+++ third.py
@@ -0,0 +1,9 @@
+#Write a program to find gross salary.
+a = int(input("enter the basic sallary :"))
+
+HRA = (20*a)/100
+DA = (50*a)/100
+TA = (5*a)/100
+
+sallary = a + HRA + DA + TA
+print("Gross sallary =",sallary)
